41aa24d304e49c90ed8bc15eaa33b6a0265fac8c
rayGreycloud <rayGreycloud@gmail.com>
2019-01-06 08:48:35 -0800

    Prettier changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,22 +10,22 @@ import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 // parameter state comes from index.js provider store state(rootReducers)
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     searchField: state.searchRobots.searchField,
     robots: state.requestRobots.robots,
-    isPending: state.requestRobots.isPending
-  }
-}
+    isPending: state.requestRobots.isPending,
+  };
+};
 
 // dispatch the DOM changes to call an action. note mapStateToProps returns object, mapDispatchToProps returns function
 // the function returns an object then uses connect to change the data from redecers.
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-    onRequestRobots: () => dispatch(requestRobots())
-  }
-}
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots()),
+  };
+};
 
 class App extends Component {
   componentDidMount() {
@@ -36,17 +36,19 @@ class App extends Component {
     const { robots, searchField, onSearchChange, isPending } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    })
+    });
     return (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={onSearchChange}/>
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
-          { isPending ? <h1>Loading</h1> :
+          {isPending ? (
+            <h1>Loading</h1>
+          ) : (
             <ErrorBoundry>
               <CardList robots={filteredRobots} />
             </ErrorBoundry>
-          }
+          )}
         </Scroll>
       </div>
     );
@@ -54,4 +56,7 @@ class App extends Component {
 }
 
 // action done from mapDispatchToProps will channge state from mapStateToProps
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
