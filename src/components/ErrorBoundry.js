96709f6f263c210b2da214dd2facf7b12c26eb92
rayGreycloud <rayGreycloud@gmail.com>
2019-01-08 06:55:35 -0800

    format corrections

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class ErrorBoundary extends Component {
-  constructor (props) {
-    super(props)
-    this.state = { hasError: false }
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+      error: null,
+      info: '',
+    };
   }
 
-  componentDidCatch (error, info) {
-    this.setState({ hasError: true })
+  componentDidCatch(error, info) {
+    this.setState({
+      hasError: true,
+      error,
+      info,
+    });
   }
 
-  render () {
+  render() {
     if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>
+      return <h1>Something went wrong.</h1>;
     }
-    return this.props.children
+    return this.props.children;
   }
 }
 
-export default ErrorBoundary
+export default ErrorBoundary;
